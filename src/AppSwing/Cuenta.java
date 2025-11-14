0477889a8a9faf00a212e0b8182e017958d752da
martin.arbulu <martin.arbulu@10.207.3.209>
2025-11-14 09:05:12 +0100

    Clase: Cuenta, Trabajador y Cliente

---
+++ src/AppSwing/Cuenta.java
@@ -0,0 +1,50 @@
+package AppSwing;
+
+
+
+public class Cuenta {
+
+    private int numCuenta;
+    private double saldo;
+    private String dni;        
+    private int numSucursal;   
+
+    public Cuenta(int numCuenta, double saldo, String dni, int numSucursal) {
+        this.numCuenta = numCuenta;
+        this.saldo = saldo;
+        this.dni = dni;
+        this.numSucursal = numSucursal;
+    }
+
+    public int getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(int numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getNumSucursal() {
+        return numSucursal;
+    }
+
+    public void setNumSucursal(int numSucursal) {
+        this.numSucursal = numSucursal;
+    }
+}
